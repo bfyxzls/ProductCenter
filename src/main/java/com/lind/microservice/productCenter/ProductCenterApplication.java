9cf4028ed2d21f8c9e8bb272157d1ad5eadd5b5c
lind.zhang <bfyxzls@sina.com>
2018-08-10 16:37:32 +0800

    添加了h2数据库主要用在测试中，mysql用在实现开发，默认运行为dev开发环境

--- src/main/java/com/lind/microservice/productCenter/ProductCenterApplication.java
+++ src/main/java/com/lind/microservice/productCenter/ProductCenterApplication.java
@@ -2,6 +2,7 @@ package com.lind.microservice.productCenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ProductCenterApplication {
